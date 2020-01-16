7456234100ea593a52d7e34743e4561048f53e85
Thales Lima <lthales53@gmail.com>
2020-01-16 20:59:19 -0300

    Starting to implement board printing method

--- XadrezConsoleCSharp/Program.cs
+++ XadrezConsoleCSharp/Program.cs
@@ -8,6 +8,7 @@ namespace XadrezConsoleCSharp
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
