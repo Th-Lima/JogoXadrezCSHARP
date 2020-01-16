b5030874da9a06c0380146ccbef17e44c4f25a47
Thales Lima <lthales53@gmail.com>
2020-01-16 19:47:46 -0300

    Creating position class

---
+++ XadrezConsoleCSharp/Tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int liha, int coluna)
+        {
+            this.linha = liha;
+            this.coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
