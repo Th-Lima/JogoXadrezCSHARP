47315c17d9abc055c674996f12c732eef32994fc
Thales Lima <lthales53@gmail.com>
2020-01-16 19:25:15 -0300

    Project initialize

---
+++ XadrezConsoleCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XadrezConsoleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
