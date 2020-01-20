e5a1e6ae6a3a15643f32092c9cc59f6ee27da67e
Thales Lima <lthales53@gmail.com>
2020-01-20 19:25:06 -0300

    Improving the implementation of the print tray method

--- XadrezConsoleCSharp/Program.cs
+++ XadrezConsoleCSharp/Program.cs
@@ -8,9 +8,23 @@ namespace XadrezConsoleCSharp
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 7));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 6));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 2));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
